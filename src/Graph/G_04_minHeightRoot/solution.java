109347083e6daaaed4ed4253713363b00f2aa36b
Upendra Khura <upendrakhura0@gmail.com>
2026-03-27 00:21:21 +0530

    Solution for minHeightRoot question

--- src/Graph/G_04_minHeightRoot/solution.java
+++ src/Graph/G_04_minHeightRoot/solution.java
@@ -15,68 +15,6 @@ public class solution {
         System.out.println("ans is : " + ans.get(0) + " " + ans.get(1));
     }
 
-//    static ArrayList<Integer> minHeightRoot(int V, int[][] edges) {
-//        ArrayList<Integer> result = new ArrayList<>();
-//
-//        if (V == 1) {
-//            result.add(0);
-//            return result;
-//        }
-//
-//        // Step 1: Build adjacency list
-//        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-//        for (int i = 0; i < V; i++) {
-//            adj.add(new ArrayList<>());
-//        }
-//
-//        int[] degree = new int[V];
-//
-//        for (int[] e : edges) {
-//            int u = e[0];
-//            int v = e[1];
-//
-//            adj.get(u).add(v);
-//            adj.get(v).add(u);
-//
-//            degree[u]++;
-//            degree[v]++;
-//        }
-//
-//        // Step 2: Add all leaf nodes (degree = 1)
-//        Queue<Integer> q = new LinkedList<>();
-//        for (int i = 0; i < V; i++) {
-//            if (degree[i] == 1) {
-//                q.offer(i);
-//            }
-//        }
-//
-//        // Step 3: Trim leaves level by level
-//        int remainingNodes = V;
-//
-//        while (remainingNodes > 2) {
-//            int size = q.size();
-//            remainingNodes -= size;
-//
-//            for (int i = 0; i < size; i++) {
-//                int node = q.poll();
-//
-//                for (int neighbor : adj.get(node)) {
-//                    degree[neighbor]--;
-//
-//                    if (degree[neighbor] == 1) {
-//                        q.offer(neighbor);
-//                    }
-//                }
-//            }
-//        }
-//
-//        // Step 4: Remaining nodes are answers
-//        while (!q.isEmpty()) {
-//            result.add(q.poll());
-//        }
-//
-//        return result;
-//    }
     static ArrayList<Integer> minHeightRoot(int V, int[][] edges) {
         List<HashSet<Integer>> AdjL = new ArrayList<>();
         for (int i = 0; i < V; i++) {
