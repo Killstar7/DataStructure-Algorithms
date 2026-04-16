f330bef4569a974e6bca71f765831c4170ea6b64
Upendra Khura <upendrakhura0@gmail.com>
2026-04-16 23:08:04 +0530

    solved remove spaces question

---
+++ src/String/S_03_Remove_Spaces/Solution.java
@@ -0,0 +1,34 @@
+package String.S_03_Remove_Spaces;
+
+/*
+  Remove Spaces
+  Difficulty: Basic
+  Given a string s, remove all the spaces from the string and return the modified string.
+
+  Examples:
+
+  Input: s = "g eeks for ge eeks"
+  Output: "geeksforgeeks"
+  Explanation: All space characters are removed from the given string while preserving the order of the remaining characters, resulting in the final string "geeksforgeeks".
+  Input: s = "abc d "
+  Output: "abcd"
+  Explanation:  All space characters are removed from the given string while preserving the order of the remaining characters, resulting in the final string "abcd".
+  Constraints:
+  1 ≤ |s| ≤ 105
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    static String removeSpaces(String s) {
+        // code here
+        String ans = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' '){
+                ans = ans + s.charAt(i);
+            }
+        }
+        return ans;
+
+    }
+}
